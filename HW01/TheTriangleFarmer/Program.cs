fe9b893303f817926a761f3a822de63a2eec3d14
James Jang <james.jang@mnsu.edu>
2025-01-31 14:13:30 -0600

    Add files via upload

---
+++ HW01/TheTriangleFarmer/Program.cs
@@ -0,0 +1,18 @@
+﻿// Written by James Jang
+// 01/27/2025
+
+namespace TheTriangleFarmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Enter height (integer value please): ");
+            int inputHeight = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter base (integer value please): ");
+            int inputBase = Convert.ToInt32(Console.ReadLine());
+            Triangle aTriangle = new Triangle(inputHeight, inputBase);
+            Console.WriteLine("Area of triangle: " + aTriangle.AreaOfTriangle() + "\n");
+        }
+    }
+}
---
+++ HW01/TheTriangleFarmer/Triangle.cs
@@ -0,0 +1,58 @@
+﻿// Written by James Jang
+// 01/27/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTriangleFarmer
+{
+    public class Triangle
+    {
+        // class variables
+        private int heightOfTriangle = -1;
+        private int baseOfTriangle = -1;
+
+        // gets and sets
+        public int HeightOfTriangle
+        {
+            get { return this.heightOfTriangle; }
+            set {
+                if (value > 0)
+                {
+                    this.heightOfTriangle = value;
+                }
+            }
+        }
+        public int BaseOfTriangle
+        {
+            get { return this.baseOfTriangle; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.baseOfTriangle = value;
+                }
+            }
+        }
+
+        // constructor
+        public Triangle(int aHeight, int aBase)
+        {
+            this.HeightOfTriangle = aHeight;
+            this.baseOfTriangle = aBase;
+        }
+        public Triangle() : this(0, 0)
+        {
+
+        }
+    
+        // class methods
+        public double AreaOfTriangle()
+        {
+            return HeightOfTriangle * BaseOfTriangle / 2.0;
+        }
+    }
+}
