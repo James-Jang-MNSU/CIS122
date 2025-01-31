b80b3da74c3d07fcbd5bcf93f0d5106de2b72885
James Jang <james.jang@mnsu.edu>
2025-01-31 14:13:01 -0600

    Add files via upload

---
+++ HW01/RepairingTheClocktower/ClockTower.cs
@@ -0,0 +1,45 @@
+﻿// Written by James Jang
+// 01/28/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairingTheClocktower
+{
+    public class ClockTower
+    {
+        // class variables
+        private int seconds = -1;
+        
+        // gets and sets
+        public int Seconds
+        {
+            get { return this.seconds; }
+            set { if (value >= 0 && value <= 60) this.seconds = value; }
+        }
+
+        // constructors
+        public ClockTower(int aNumOfSeconds)
+        {
+            if (aNumOfSeconds >= 0 && aNumOfSeconds <= 60) this.seconds = aNumOfSeconds;
+        }
+
+        // class methods
+        public string TickOrTock()
+        {
+            if (seconds % 2  == 0)
+            {
+                return "tick";
+            }
+            else
+            {
+                return "tock";
+            }
+        }
+
+
+    }
+}
---
+++ HW01/RepairingTheClocktower/Program.cs
@@ -0,0 +1,16 @@
+﻿// Written by James Jang
+// 01/28/2025
+
+namespace RepairingTheClocktower
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of seconds (between 0 and 60): ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            ClockTower aClockTower = new ClockTower(inputNumber);
+            Console.WriteLine(aClockTower.TickOrTock());
+        }
+    }
+}
