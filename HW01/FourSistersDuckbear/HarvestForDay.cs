8d284c6bf0461cebc6ed745c3a3157e9c1d6c831
James Jang <james.jang@mnsu.edu>
2025-01-31 14:12:35 -0600

    Add files via upload

---
+++ HW01/FourSistersDuckbear/HarvestForDay.cs
@@ -0,0 +1,43 @@
+﻿// Written by James Jang
+// 01/27/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourSistersDuckbear
+{
+    public class HarvestForDay
+    {
+        // class variables
+        private int totalChocolateEgss = -1;
+
+        // gets and sets
+        public int TotalChocolateEgss
+        {
+            get { return totalChocolateEgss; }
+            set { totalChocolateEgss = value; }
+        }
+
+        // constructors
+        public HarvestForDay(int aNumOfChocolateEgss)
+        {
+            this.totalChocolateEgss = aNumOfChocolateEgss;
+        }
+
+        public HarvestForDay() : this(0) { }
+
+        // class methods
+        public int EggsForDuckbear()
+        {
+            return totalChocolateEgss % 4;
+        }
+        public int EggsPerPerson()
+        {
+            return (totalChocolateEgss - EggsForDuckbear()) / 4;
+        }
+
+    }
+}
---
+++ HW01/FourSistersDuckbear/Program.cs
@@ -0,0 +1,17 @@
+﻿// Written by James Jang
+// 01/27/2025
+
+namespace FourSistersDuckbear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of chocolate eggs: ");
+            int numOfEggs = Convert.ToInt32(Console.ReadLine());
+            HarvestForDay aHarvestForDay = new HarvestForDay(numOfEggs);
+            Console.WriteLine("Eggs per person: " + aHarvestForDay.EggsPerPerson());
+            Console.WriteLine("Eggs for duckbear: " + aHarvestForDay.EggsForDuckbear() + "\n");
+        }
+    }
+}
