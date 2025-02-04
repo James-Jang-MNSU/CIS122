3eca8ae58bcf3962453206dc41e5619c6b383690
James Jang <james.jang@mnsu.edu>
2025-02-04 15:48:54 -0600

    Add files via upload

---
+++ HW02/ThePrototype/Program.cs
@@ -0,0 +1,17 @@
+﻿// Written James Jang
+// 02/04/2025
+
+namespace ThePrototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Locate the airship: ");
+            int airshipLocation = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            TargetLocater aTargetLocater = new TargetLocater(airshipLocation);
+            aTargetLocater.GuessLocation();
+        }
+    }
+}
---
+++ HW02/ThePrototype/TargetLocater.cs
@@ -0,0 +1,53 @@
+﻿// Written James Jang
+// 02/04/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePrototype
+{
+    public class TargetLocater
+    {
+        // class variables
+        private int targetLocation = int.MaxValue;
+
+        // gets and sets
+        public int TargetLocation
+        {
+            get { return this.targetLocation; }
+            // readonly
+        }
+
+        // constructors - full constructor only
+        public TargetLocater(int aLocation) 
+        { 
+            if (aLocation >= 0 && aLocation <= 100)
+            {
+                this.targetLocation = aLocation;
+            }
+        }
+
+        // class methods
+        public void GuessLocation()
+        {
+            Console.Write("Guess the location of the target: ");
+            int huntersGuess = Convert.ToInt32(Console.ReadLine());
+            while (huntersGuess != TargetLocation)
+            {
+                if (huntersGuess > TargetLocation)
+                {
+                    Console.Write($"{huntersGuess} is too high! Try again: ");
+                }
+                else
+                {
+                    Console.Write($"{huntersGuess} is too low! Try again: ");
+                }
+                huntersGuess = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("You've located the target!");
+        }
+    }
+}
