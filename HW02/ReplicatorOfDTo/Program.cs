7836324730131b7ccbf317a50bf37c0f081b22e9
James Jang <james.jang@mnsu.edu>
2025-02-04 15:48:13 -0600

    Add files via upload

---
+++ HW02/ReplicatorOfDTo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Written by James Jang
+// 02/04/2025
+
+namespace ReplicatorOfDTo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 numbers for your array");
+            int arrayEntry1 = Convert.ToInt32(Console.ReadLine());
+            int arrayEntry2 = Convert.ToInt32(Console.ReadLine());
+            int arrayEntry3 = Convert.ToInt32(Console.ReadLine());
+            int arrayEntry4 = Convert.ToInt32(Console.ReadLine());
+            int arrayEntry5 = Convert.ToInt32(Console.ReadLine());
+            ReplicatorOfDTo aReplicator = new ReplicatorOfDTo(arrayEntry1, arrayEntry2, arrayEntry3, arrayEntry4, arrayEntry5);
+            aReplicator.ReplicateArray();
+
+        }
+    }
+}
---
+++ HW02/ReplicatorOfDTo/ReplicatorOfDTo.cs
@@ -0,0 +1,46 @@
+﻿// Written James Jang
+// 02/04/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplicatorOfDTo
+{
+    public class ReplicatorOfDTo
+    {
+        // class variables
+        private int[] originalArray;
+
+        // gets and sets
+        public int[] OriginalArray
+        {
+            get { return this.originalArray; }
+            set { this.originalArray = value; }
+        }
+
+        // constructors
+        public ReplicatorOfDTo(int num1, int num2, int num3, int num4, int num5)
+        {
+            this.originalArray = new int[] {num1, num2, num3, num4, num5};
+        }
+        
+        // class methods
+        public void ReplicateArray()
+        {
+            int[] replica = new int[originalArray.Length];
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                replica[i] = OriginalArray[i]; 
+            }
+            Console.WriteLine("Index\tOriginal\tReplica");
+            for (int i = 0; i < replica.Length; i++)
+            {
+                Console.WriteLine($"{i}\t{OriginalArray[i]}\t\t{replica[i]}");
+            }
+        }
+        
+    }
+}
