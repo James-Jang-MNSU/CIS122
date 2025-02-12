1601ef82431ff5ba2a954a6bcb331bbdb9fa73b6
James Jang <james.jang@mnsu.edu>
2025-02-12 15:59:15 -0600

    Add files via upload

---
+++ HW03/SmallerNum/Calculator.cs
@@ -0,0 +1,20 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerNum
+{
+    public class Calculator
+    {
+        // class methods
+        public string SmallerNum(string num1, string num2)
+        {
+            return (Convert.ToInt32(num1) < Convert.ToInt32(num2)) ? num1 : num2;
+        }
+    }
+}
---
+++ HW03/SmallerNum/Program.cs
@@ -0,0 +1,18 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+namespace SmallerNum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator aCalculator = new Calculator();
+            Console.Write("Enter an integer: ");
+            string userNum1 = Console.ReadLine();
+            Console.Write("Enter another integer: ");
+            string userNum2 = Console.ReadLine();
+            Console.WriteLine($"The smaller number is {aCalculator.SmallerNum(userNum1, userNum2)}");
+        }
+    }
+}
