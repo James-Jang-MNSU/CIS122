0e08370ac34b1f1c5292a1bbe2d9f52ee8ec1dda
James Jang <james.jang@mnsu.edu>
2025-02-12 15:56:20 -0600

    Add files via upload

---
+++ HW03/FindMinMax/Calculator.cs
@@ -0,0 +1,26 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMinMax
+{
+    public class Calculator
+    {
+        // class methods
+        public (int, int) FindMinMax(int[] anArrayNums)
+        {
+            int min = anArrayNums[0], max = anArrayNums[0];
+            foreach (int number in anArrayNums)
+            {
+                if (number < min) min = number;
+                if (number > max) max = number;
+            }
+            return (min, max);
+        }
+    }
+}
---
+++ HW03/FindMinMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+namespace FindMinMax
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator aCalculator = new Calculator();
+            int[] anArrayOfNumbers = { 1, 2, 3, 4, 5 };
+            (int minimum, int maximum) = aCalculator.FindMinMax(anArrayOfNumbers);
+            Console.WriteLine($"minimum: {minimum}, maximum: {maximum}");
+        }
+    }
+}
