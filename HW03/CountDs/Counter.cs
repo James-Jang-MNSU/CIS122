7ded1efdd1307b253a73111cbe96db0e363d0d04
James Jang <james.jang@mnsu.edu>
2025-02-12 15:55:30 -0600

    Add files via upload

---
+++ HW03/CountDs/Counter.cs
@@ -0,0 +1,25 @@
+﻿// Written by James Jang
+// 02/10/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDs
+{
+    public class Counter
+    {
+        // class methods
+        public int CountDs(string aString)
+        {
+            int count = 0;
+            foreach (char letter in aString)
+            {
+                if (letter == 'D' || letter == 'd') count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ HW03/CountDs/Program.cs
@@ -0,0 +1,15 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+namespace CountDs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Counter aCounter = new Counter();
+            string testString = "My friend Dylan got distracted in school.";
+            Console.WriteLine(aCounter.CountDs(testString));
+        }
+    }
+}
