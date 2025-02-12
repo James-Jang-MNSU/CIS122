f818c1983a92be0e900ed41d9f5048ec87942c60
James Jang <james.jang@mnsu.edu>
2025-02-12 15:57:44 -0600

    Add files via upload

---
+++ HW03/MultiplyByLength/Calculator.cs
@@ -0,0 +1,25 @@
+﻿// Written by James Jang
+// 02/10/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyByLength
+{
+    public class Calculator
+    {
+        // class methods
+        public int[] MultiplyByLength(int[] anArrayOfNums)
+        {
+            int[] result = new int[anArrayOfNums.Length];
+            for (int i = 0; i < anArrayOfNums.Length; i++)
+            {
+                result[i] = anArrayOfNums[i] * anArrayOfNums.Length;
+            }
+            return result;
+        }
+    }
+}
---
+++ HW03/MultiplyByLength/Program.cs
@@ -0,0 +1,18 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+namespace MultiplyByLength
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator aCalculator = new Calculator();
+            int[] anArrayOfNumbers = { 2, 3, 1, 0 };
+            int[] modifiedArrayOfNumebers = aCalculator.MultiplyByLength(anArrayOfNumbers);
+            Console.Write("[ ");
+            foreach (int number in modifiedArrayOfNumebers) { Console.Write(number + ", "); }
+            Console.WriteLine("\b\b ]");
+        }
+    }
+}
