3ea8a2e77bc870e42faa077c30d7ced695fd78ad
James Jang <james.jang@mnsu.edu>
2025-02-12 16:00:03 -0600

    Add files via upload

---
+++ HW03/VinsTrouble/Program.cs
@@ -0,0 +1,20 @@
+﻿// Written by James Jang
+// 02/11/2025
+
+namespace VinsTrouble
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Select your arrow head type (steel/wood/obsidian): ");
+            string userHeadType = Console.ReadLine();
+            Console.Write("Select your arrow length (60-100): ");
+            int userLength = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Select your arrow fletching type (plastic/turkey feathers/goose feathers): ");
+            string userFletchingType = Console.ReadLine();
+            Arrow userArrow = new Arrow(userHeadType, userLength, userFletchingType);
+            Console.WriteLine($"Your arrow costs {userArrow.GetCost()} gold");
+        }
+    }
+}
