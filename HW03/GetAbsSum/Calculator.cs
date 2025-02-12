1a7eb702fabf18b34872c8965f19c92a33727de5
James Jang <james.jang@mnsu.edu>
2025-02-12 15:56:40 -0600

    Add files via upload

---
+++ HW03/GetAbsSum/Calculator.cs
@@ -0,0 +1,32 @@
+﻿// Written by James Jang
+// 02/11/2025 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAbsSum
+{
+    public class Calculator
+    {
+        // class methods
+        public int GetAbsSum(int[] arrayOfNums)
+        {
+            int sum = 0;
+            foreach (int num in arrayOfNums)
+            {
+                if (num < 0)
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ HW03/GetAbsSum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Written by James Jang
+// 02/11/2025 
+
+namespace GetAbsSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator aCalculator = new Calculator();
+            int[] anArrayOfNumebrs = { -3, -4, -10, -2, -3 };
+            Console.WriteLine(aCalculator.GetAbsSum(anArrayOfNumebrs));
+        }
+    }
+}
